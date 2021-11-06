e944cfa54c5ef868010e050bb4356b1dfe0fb5b5
Itxcoatl <nairda_itzcoatl@hotmail.com>
2021-11-06 15:28:19 -0600

    Video37

--- client/index.js
+++ client/index.js
@@ -1,4 +1,12 @@
-/* eslint-disable no-console */
 /* eslint-disable no-alert */
 import './stylesheets/style.css';
 import './stylesheets/mystyles.css';
+
+// eslint-disable-next-line no-console
+console.log('webpack working');
+// inicializando scrip de materialize
+document.addEventListener('DOMContentLoaded', () => {
+  const sideNav = document.querySelectorAll('.sidenav');
+  // eslint-disable-next-line no-undef
+  M.Sidenav.init(sideNav);
+});
