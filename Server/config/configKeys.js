afa96d5918845cc0ab8b9d85d704ee7477f780c5
Itxcoatl <nairda_itzcoatl@hotmail.com>
2021-11-06 16:35:38 -0600

    Video38

---
+++ Server/config/configKeys.js
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+// improtando el paquete de DotEnv
+import dotenv from 'dotenv';
+// Cargando las variables de entorno
+dotenv.config();
+
+// Exportando los valores de configuracion
+export default {
+  homeUrl: `${process.env.APP_URL}:${process.env.PORT}`,
+  port: process.env.PORT,
+  ip: process.env.IP,
+};
